81d57f8f79e8683926b17a7bb50e37d325045390
TrevorKimble <tvkimble04@gmail.com>
2022-11-11 08:53:19 -0500

    made working form

---
+++ login.js
@@ -0,0 +1,14 @@
+function add_info(input)
+{
+    console.log(document.getElementById("name").value);
+    console.log(document.getElementById("email").value);
+    console.log(document.getElementById("password").value);
+}
+
+function check()
+{
+
+}
+
+
+var data = [["username","password","name"]];
