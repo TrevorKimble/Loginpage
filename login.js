e82178e561f5a37d56782be60c482f105340bc43
TrevorKimble <tvkimble04@gmail.com>
2022-11-14 10:59:35 -0500

    stuff

--- login.js
+++ login.js
@@ -4,20 +4,17 @@ function add_info(input)
     let a = document.getElementById("name").value;
     let b = document.getElementById("email").value;
     let c = document.getElementById("password").value;
-    let d = document.getElementById("birthday").value;
-    console.log(pass);
     pass = check(a,b);
-    console.log(pass);
     if(pass === true)
     {
         names.push(a);
         user_names.push(b);
         passwords.push(c);
     }
+    console.log(names);
 }
 function check(a,b)
 {
-    console.log("test")
     for(x = 0; x < names.length; x++)
     {
         if(a == names[x])
@@ -36,15 +33,19 @@ function check(a,b)
 
 function log_in()
 {
-    let a = document.getElementById("name").value;
-    let b = document.getElementById("email").value;
-    let c = document.getElementById("password").value;
+    console.log(names);
+    let a = document.getElementById("name2").value;
+    let b = document.getElementById("email2").value;
+    let c = document.getElementById("password2").value;
 
-    for(x = 0; x < names.length; x++)
+    for(x = 0; x < 100; x++)
     {
         if((a == names[x]) && (b == user_names[x]) && (c == passwords[x]))
         {
             window.location.href="landing_page.html"+window.location.href;
+        }
+        else{
+            console.log("fail");
         }
     }
 }
