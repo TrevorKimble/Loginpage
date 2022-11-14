e64e6850dd9be0f512be644fe85646b0a89f773f
TrevorKimble <tvkimble04@gmail.com>
2022-11-14 10:32:53 -0500

    landing page

--- login.js
+++ login.js
@@ -34,8 +34,22 @@ function check(a,b)
     return true;
 }
 
+function log_in()
+{
+    let a = document.getElementById("name").value;
+    let b = document.getElementById("email").value;
+    let c = document.getElementById("password").value;
+
+    for(x = 0; x < names.length; x++)
+    {
+        if((a == names[x]) && (b == user_names[x]) && (c == passwords[x]))
+        {
+            window.location.href="landing_page.html"+window.location.href;
+        }
+    }
+}
+
 
 var  names = [];
 var  user_names = [];
 var  passwords = [];
-
